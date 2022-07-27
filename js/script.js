0e86d7ff99341c79add206b67aa84898115b5cd3
Jada Jones <jadasjones86@gmail.com>
2022-07-27 09:57:27 -0400

    lots of changes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// Hamburger Menu //
+
+    const toggleButton = document.getElementById('toggleButton');
+    const navLinks = document.getElementById('nav-links');
+
+    toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle('activate');
+    })
+
+
+
+
