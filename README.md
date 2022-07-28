8087f2e2d9909b74bcfff44d4a05374ce24f6315
Jada Jones <jadasjones86@gmail.com>
2022-07-28 09:29:13 -0400

    lots of changes, add README

---
+++ README.md
@@ -0,0 +1,13 @@
+### Description:
+Cuisine Insights is built with HTML5, CSS and JavaScript. It boasts a clean and neutral theme. The website aims to showcase the cuisines of different countries. It is geared to foodies, those interested in exploring their heritage or those who enjoy learning about other cultures. This site plans to include profile pages for a variety of countries. Each profile page would explore the common flavors/ingredients prevalent in the country’s cuisine, and the national and popular dishes and desserts. 
+
+
+### Features:
+* Use of Flexbox and CSS Grid for layout and organization of content. (CSS Feature)
+* A  responsive navigation menu that expands and collapses based on screen size. (JavaScript Feature)
+* Contact form with 3 fields, name, email, and message (all of which contain validation rules) and generates a "Thank you" message upon submission that captures the name. (JavaScript Feature)
+
+
+
+**Major change from original project plan: A profile page for each featured country has not been implemented.*
+ 
--- js/contactForm.js
+++ js/contactForm.js
@@ -9,19 +9,32 @@ const errorNodes = document.querySelector(".error");
 
 function validateForm(){
 
-     clearMessages();
+    //  clearMessages();
 
     if(nameInput.value.length < 1){
-        errorNodes[0].innerText="Name cannot be blank";
-        nameInput.classList.add ("error-border");
+        alert("Name cannot be blank")
+       
+    } else if (email.value.length<1) {
+        alert("Email cannot be blank")
+    } else if (message.value.length<1){
+        alert("Message cannot be blank")
+    } else {
+        if (email.value.includes("@")){
+            alert(`Thank you for your message ${nameInput.value}`)
+        }else{
+            alert(`Your email is not in the correct format :  ${email.value}`)
+        }
     }
+
+
+    
     
 }
 
-function clearMessages() {
-    for(let i=0; i < errorNodes.length; i++) {
-        errorNodes[i].innerText="";
+// function clearMessages() {
+//     for(let i=0; i < errorNodes.length; i++) {
+//         errorNodes[i].innerText="";
 
-    }
-    nameInput.classList.remove("error-border");
-}
+//     }
+//     nameInput.classList.remove("error-border");
+// }
